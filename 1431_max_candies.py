a1f7db191d671da1021ff467ef0000ad370c98ba
Dushyant Kumar <157031778+d95-exe@users.noreply.github.com>
2025-02-14 12:08:21 +0530

    Update 1431_max_candies.py
    edited to be able to run locally

--- 1431_max_candies.py
+++ 1431_max_candies.py
@@ -2,3 +2,9 @@ class Solution:
     def kidsWithCandies(self, candies, extraCandies):
         maxCandy = max(candies)
         return [candy + extraCandies >= maxCandy for candy in candies]
+
+sol = Solution()
+candies = [2, 3, 5, 1, 3]
+extraCandies = 3
+
+print(sol.kidsWithCandies(candies, extraCandies))
